1db3d1fe544c940fe9df0e8db7f889db6d65fb7d
Your Name <you@example.com>
2016-05-23 10:19:25 +0200

    skeleton of github deploy tests

--- test/test_create_project.sh
+++ test/test_create_project.sh
@@ -6,6 +6,12 @@ set -e
 
 TOXENV=$1
 
+DEPLOY=1
+if [ -z "$2" ]
+  then
+    DEPLOY=0
+fi
+
 require() {
     type $1 >/dev/null 2>/dev/null
 }
@@ -36,6 +42,10 @@ cookiecutter . --no-input
     PYTHONPATH=. python name_of_the_project/run_name_of_the_project.py
     PYTHONPATH=. python name_of_the_project/run_name_of_the_project.py --verbose
     tox -e $TOXENV -- -n 8
+    if [ "DEPLOY" ]
+    then
+        echo "TODO REPO DEPLOY"
+    fi
 )
 
 echo Done
