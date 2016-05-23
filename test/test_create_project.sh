c73b20eda922fc01f56509bd0b4b45ba23cde21e
Leszek Grzanka <Leszek.Grzanka@ifj.edu.pl>
2016-05-23 20:58:27 +0200

    fixed github deployment and travis builder activation

--- test/test_create_project.sh
+++ test/test_create_project.sh
@@ -2,6 +2,8 @@
 
 # based on https://github.com/audreyr/cookiecutter-pypackage/
 
+set -x # Print command traces before executing command
+
 set -e # Exit immediately if a simple command exits with a non-zero status.
 
 set -o pipefail # Return value of a pipeline as the value of the last command to
@@ -16,8 +18,8 @@ if [ -z "$2" ]
     DEPLOY=0
 fi
 
-
-PROJNAME="cookie03"
+TMPDIR=`mktemp -d`
+PROJNAME="cookie05"
 EMAIL="grzanka@agh.edu.pl"
 NAME="Leszek Grzanka"
 GITHUBUSER="grzankatest"
@@ -28,16 +30,12 @@ require() {
 
 cleanup() {
     rm -rf $PROJNAME
-    rm github_deploy_config.json
+    rm -f $TMPDIR/github_deploy_config.json
 }
 trap cleanup EXIT
 
-
-require cookiecutter
-
-cd test
-
-cat <<EOT >> github_deploy_config.json
+prepare_cookie_config() {
+    cat <<EOT > $1
 default_context:
     full_name: $NAME
     email: $EMAIL
@@ -45,65 +43,119 @@ default_context:
     project_name: $PROJNAME
     requiresio: "yes"
 EOT
+}
+
+setup_travis() {
+        # install travis client
+        ruby -v
+        gem install travis -q --no-rdoc --no-ri
+        travis version
+
+        echo "Logging to travis"
+        # disable printing bash command to hide GITHUBTOKEN
+        set +x
+        travis login --org -g $GITHUBTOKEN -u $GITHUBUSER --no-manual --no-interactive
+        set -x
+        travis whoami
+
+        # syncing repos
+        travis sync
+
+        # listing repos
+        travis repos
+
+        # enabling repo in travis
+        travis enable -r $GITHUBUSER/$GITHUBREPO
+
+        # listing repos
+        travis repos
+}
 
 
+deploy_to_github() {
+        GITHUBREPO=python-$PROJNAME
+
+        # remove repo, if exists
+        echo "Attempt to delete repo $GITHUBREPO (username $GITHUBUSER)"
+        # disable printing bash command to hide GITHUBTOKEN
+        set +x
+        curl -X DELETE -H "Authorization: token $GITHUBTOKEN" https://api.github.com/repos/$GITHUBUSER/$GITHUBREPO
+        sleep 5
+
+        # create repo
+        echo "Creating repo $GITHUBREPO (username $GITHUBUSER)"
+        curl -u "$GITHUBUSER:$GITHUBTOKEN" https://api.github.com/user/repos -d "{\"name\":\"$GITHUBREPO\"}"
+        sleep 5
+
+        # enable travis for this newly created repo
+        setup_travis
+
+        # save credentials in a store, this way "git push" won't ask for a password
+        git config --global credential.helper store
+        cat <<EOF > ~/.git-credentials
+https://$GITHUBUSER:$GITHUBTOKEN@github.com
+EOF
+        set -x
+
+        git remote add origin https://github.com/$GITHUBUSER/$GITHUBREPO.git
+
+        # now move to directory containing repo and push it to remote
+        cd $1
+        git push -u origin master
+
+        # add informative header to generated travis file
+        TMPFILE=mktemp
+        cp README.rst $TMPFILE
+        echo "Travis generated this project on:" `date` > README.rst
+        echo "" >> README.rst
+        echo "Github commit https://github.com/grzanka/cookiecutter-pip-docker-versioneer/commit/"$TRAVIS_COMMIT >>  README.rst
+        echo "" >> README.rst
+        cat $TMPFILE >> README.rst
+
+        # commiting modified readme and pushing to remote
+        git add README.rst
+        git commit -m "README updated"
+        git push -u origin master
+}
+
+require cookiecutter
+
+# save current directory
+CURDIR=`pwd`
+
+# move to temporary directory, to be save and not clutter original workspace
+cd $TMPDIR
+
+prepare_cookie_config $TMPDIR/github_deploy_config.json
 echo "Running test script..."
-cookiecutter --config-file github_deploy_config.json --no-input ..
+cookiecutter --config-file $TMPDIR/github_deploy_config.json --no-input $CURDIR
 (
     cd ./$PROJNAME
+
+    # creating git repository here and adding all files
     export HOME=`pwd`
     git config --global user.email $EMAIL
     git config --global user.name $NAME
     git init .
     git add -A .
     git commit -m "initial commit"
+
+    # generating versioneeer files
     pip install versioneer
     pip install -r requirements.txt
     versioneer install
+
+    # running necessary tests
     PYTHONPATH=. python $PROJNAME/run_$PROJNAME.py --help
     PYTHONPATH=. python $PROJNAME/run_$PROJNAME.py --version
     PYTHONPATH=. python $PROJNAME/run_$PROJNAME.py
     PYTHONPATH=. python $PROJNAME/run_$PROJNAME.py --verbose
     tox -e $TOXENV -- -n 8
+
+    # if github deploy enabled, put this repository to github and run travis tests
     if [[ $DEPLOY -eq 1 ]]
     then
-        GITHUBREPO=python-$PROJNAME
-
-        # remove repo, if exists
-        echo "Attempt to delete repo $GITHUBREPO (username $GITHUBUSER)"
-        curl -X DELETE -H "Authorization: token $GITHUBTOKEN" https://api.github.com/repos/$GITHUBUSER/$GITHUBREPO
-
-        sleep 10
-
-        # create repo
-        echo "Creating repo $GITHUBREPO (username $GITHUBUSER)"
-        curl -u "$GITHUBUSER:$GITHUBTOKEN" https://api.github.com/user/repos -d "{\"name\":\"$GITHUBREPO\"}"
-
-        sleep 10
-
-        # save credentials in a store, this way "git push" won't ask for a password
-        git config --global credential.helper store
-        echo "https://$GITHUBUSER:$GITHUBTOKEN@github.com" > ~/.git-credentials
-
-        git remote add origin https://github.com/$GITHUBUSER/$GITHUBREPO.git
-
-        ## clean remote repo and set its content to the local one
-        # git push origin --mirror
-
-        git push -u origin master
-
-        # add timestamp to generated travis file
-        TMPFILE=mktemp
-        cp README.rst $TMPFILE
-        echo "Travis generated this project on:" `date` > README.rst
-        echo "" >> README.rst
-        cat $TMPFILE >> README.rst
-
-        git add README.rst
-        git commit -m "README updated"
-
-        git push -u origin master
-
+        deploy_to_github `pwd`
     fi
 )
 
